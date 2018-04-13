fbacfe0b1fe313643e560dcaa47da2d2bec9e487
Chaktim Wong <cwong10@stuy.edu>
2018-04-12 20:05:05 -0400

    version 0 - bugged

--- MazeSolver.java
+++ MazeSolver.java
@@ -8,13 +8,55 @@
    -Backtrack when no path available
 
 */
-
-
-
 public class MazeSolver {
 
-    public MazeSolver ( String sourceFilename
-               , int explorerRank, int explorerFile
-               ) throws java.io.FileNotFoundException {
-	Maze mazeToBeSolved = new Maze(sourceFilename, explorerRank, explorerFile);
+    //for now, public
+    public Maze mazeToBeSolved;
+    
+    public MazeSolver (Maze maze) {
+    //not sure if this is needed
+    //throws java.io.FileNotFoundException {
+	mazeToBeSolved = new Maze(maze);
     }
+
+    public String toString(){
+	if (solve()) return mazeToBeSolved.toString();
+	else return "no solution";
+    }
+    
+    private boolean onTreasure() {
+        return mazeToBeSolved.explorerIsOnA() == 0;
+    }
+    private boolean onWall() {
+       return mazeToBeSolved.explorerIsOnA() == 1;
+    } 
+
+    public boolean solve() {
+	//base case
+        if (onTreasure()) return true;
+	else {
+	    //move foward, one direction at a time
+	    for(int direction = 1; direction <= 8; direction*=2) {
+		Maze snapshot = new Maze(mazeToBeSolved);
+
+		//drop wall to prevent infinite loop
+		mazeToBeSolved.dropA(1);
+
+		//move foward
+		mazeToBeSolved.go(direction);
+		
+		//if on wall, backtrack using snapshot
+		if(onWall()) {
+		    mazeToBeSolved = snapshot;
+		    if(direction == 8) return false;
+		}
+		//otherwise, continue on this path (recursive)
+		else if(solve()) return true;
+	    }
+	}
+	return false;
+    }
+}
+		    
+		
+
